73636dd8c4cf5849f859007b574435088d0587ae
Victor Vasconcelos <victor.vasconcelosj@gmail.com>
2026-09-08 17:29:08 -0600

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,43 @@
+class WordNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = WordNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = WordNode()
+
+            curr = curr.children[c]
+
+        curr.word = True
+        
+
+    def search(self, word: str) -> bool:
+    
+        def backtrack(node, i):
+
+            if i == len(word):
+                return node.word
+
+            c = word[i]
+
+            if c == '.':
+                for child in node.children.values():
+                    if backtrack(child, i + 1):
+                        return True
+            elif c in node.children:
+                return backtrack(node.children[c], i + 1)
+
+            return False
+
+        return backtrack(self.root, 0)
+
