2525327cca286a73c044ecc48064cd9437fe4fd5
Victor Vasconcelos <victor.vasconcelosj@gmail.com>
2026-09-17 07:42:16 -0600

    Add: climbing-stairs - submission-12

---
+++ Data Structures & Algorithms/climbing-stairs/submission-12.py
@@ -0,0 +1,42 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        def brute(n):
+            if n == 2:
+                return 2
+            if n == 1:
+                return 1
+
+            return brute(n - 1) + brute(n - 2)
+
+        cache = {}
+        # top - bottom
+        def memo(n):
+            if n == 2:
+                return 2
+            if n == 1:
+                return 1
+
+            if n in cache:
+                return cache[n]
+
+            cache[n] =  memo(n - 1) + memo(n - 2)
+
+            return cache[n]
+
+        # bottom - up
+        def dp(n):
+            if n == 1:
+                return 1
+                
+            one , two = 1, 2
+
+            for i in range(n - 2):
+                one, two = two, one + two
+
+
+            return two
+
+
+        return dp(n)
+        
